14b78313af543f0ab44861a93d7edb316f36c817
David <davidbarrenechea98@gmail.com>
2024-09-30 00:31:47 +0300

    added checks for valid dates

--- ex00/srcs/BitCoinExchange.cpp
+++ ex00/srcs/BitCoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: dbarrene <dbarrene@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 14:53:32 by dbarrene          #+#    #+#             */
-/*   Updated: 2024/09/29 18:43:18 by dbarrene         ###   ########.fr       */
+/*   Updated: 2024/09/30 00:16:56 by dbarrene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static	bool dateValidity(std::string line)
 		DAY
 	};
 	std::string ymd[3]; // year, month, day
-	int		validity;
+	int		yr, mo, day;
 	int		index = 0;
 	line = line.substr(0, line.find(','));
 	for (int i = 0; i < 3; i++)
@@ -32,8 +32,8 @@ static	bool dateValidity(std::string line)
 				ymd[YEAR] = line;
 				index = ymd[YEAR].find('-');
 				ymd[YEAR] = ymd[YEAR].substr(0, ymd[YEAR].find('-'));
-				validity = std::stoi(ymd[YEAR]);
-				if (validity < 2009 || ymd[YEAR].length() != 4)
+				yr = std::stoi(ymd[YEAR]);
+				if (yr < 2009 || ymd[YEAR].length() != 4)
 					return false;
 				std::cout << "year: " << ymd[YEAR] << std::endl;
 				break;
@@ -41,30 +41,51 @@ static	bool dateValidity(std::string line)
 				ymd[MONTH] = line;
 				ymd[MONTH] = ymd[MONTH].substr(ymd[MONTH].find('-', index) + 1, 2);
 				std::cout << "month: " << ymd[MONTH] << "\n";
-				validity = std::stoi(ymd[MONTH]);
-				if (ymd[MONTH].length() != 2 || validity <= 0 || validity > 12)
+				mo = std::stoi(ymd[MONTH]);
+				if (ymd[MONTH].length() != 2 || mo <= 0 || mo > 12)
 					return false;
 				break;
 			case DAY:
 				ymd[DAY] = line;
 				ymd[DAY] = ymd[DAY].substr(ymd[DAY].find_last_of('-') + 1, ymd[DAY].find(',') - 1);
 				std::cout << "day: " << ymd[DAY] << "\n";
-				validity = std::stoi(ymd[DAY]);
-				if (ymd[DAY].length() != 2 || validity <= 0 || validity > 31)
+				day = std::stoi(ymd[DAY]);
+				if (ymd[DAY].length() != 2 || day <= 0 || day > 31)
 					return false;
+				if ((mo < 8 && mo % 2 == 1) || (mo >= 8 && !mo % 2))
+				{
+					if (day > 31)
+						return false;
+				}
+				else if (mo == 2)
+				{
+					if (!yr % 4 && day > 29)
+						return false;
+					else if (day > 28)
+						return false;
+				}
+				else
+					if (day > 30)
+						return false;
 				break;
 		}
+		std::cout << "date is indeed valid \n";
+		return true;
 	} 
 	//TODO:
+	//months 1, 3, 5, 7, 8, 10, 12 have 31 days
 	//validate that the days are not too large for corresponding months,
 	//consider leap years
 	//validate that the amount after the date fits the subject
 	//add valid dates + amounts to map
+
 	return true;
 }
 
 static bool	lineValidity(std::string line)
 {
+	if (line.find('-') != 4 && line.find_last_of('-' != 7))
+			return false;
 	if (line.find(',') == SIZE_MAX)
 			return false;
 	return true;
@@ -83,12 +104,15 @@ void BitCoinExchange::parseExchange()
 	std::ifstream input("data.csv");
 	std::string line;
 	std::getline(input, line);
-	std::getline(input, line);
-	std::cout << line << std::endl;
-	if (!lineValidity(line))
-		throw std::runtime_error("bad line");
-	if (!dateValidity(line))
-		throw std::runtime_error("bad line");
+
+	while (std::getline(input, line))
+	{
+		std::cout << line << std::endl;
+		if (!lineValidity(line))
+			throw std::runtime_error("bad line");
+		if (!dateValidity(line))
+			throw std::runtime_error("bad line");
+	}
 
 };
 
